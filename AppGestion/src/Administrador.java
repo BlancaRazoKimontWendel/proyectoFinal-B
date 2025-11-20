71057dc2e84c1a2990b756a5c9a2e7a151b185a9
Brayan Montiel <braymontiel77@gmail.com>
2025-11-19 19:44:16 -0600

    Se establecen comentarios genéricos y reglas de uso en las clases

--- AppGestion/src/Administrador.java
+++ AppGestion/src/Administrador.java
@@ -1,3 +1,15 @@
-public class Administrador extends Usuario {
+import java.util.UUID;
 
+/**
+ * ----- Mensaje genérico -----
+ * Clase administrada sólo por BRAYAN.
+ * Puede modificarse por alguien más SÓLO en caso de que quiera hacer pruebas, pero debe
+ * regresarse a su estado original. En caso de que no se reestablezca a su estado original,
+ * el pull request será rechazado.
+ * ----- Mensaje genérico -----
+ * 
+ * INSERTA TU PROPIO COMENTARIO DE LA CLASE.
+ */
+public class Administrador extends Usuario {
+    
 }
--- AppGestion/src/Desarrollador.java
+++ AppGestion/src/Desarrollador.java
@@ -1,3 +1,13 @@
+/**
+ * ----- Mensaje genérico -----
+ * Clase administrada sólo por BLANCA.
+ * Puede modificarse por alguien más SÓLO en caso de que quiera hacer pruebas, pero debe
+ * regresarse a su estado original. En caso de que no se reestablezca a su estado original,
+ * el pull request será rechazado.
+ * ----- Mensaje genérico -----
+ * 
+ * INSERTA TU PROPIO COMENTARIO DE LA CLASE.
+ */
 public class Desarrollador extends Usuario {
 
 }
